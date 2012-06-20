01831c7638cbb7dee38cb94b7664f035fc31bed2
lleveroni <lleveroni@eryxsoluciones.com.ar>
2012-06-20 04:12:36 -0300

    Player: Varias mejoras de funcionamiento y algo de refactoring de código (fix #658).

--- discazos/albums/static/js/discazos_player.js
+++ discazos/albums/static/js/discazos_player.js
@@ -1,67 +1,66 @@
 var DiscazosPlayer = {};
+var DiscazosPlayerUI = {}
 
 DiscazosPlayer.MIN_PERCENTAGE_TO_LOAD = 2;
 
 $(document).ready(function() {
   DiscazosPlayer.swf = swfobject.getObjectById("discazos-player-swf");
-  DiscazosPlayer.html = $("#discazos-player-html");
+  DiscazosPlayerUI.html = $("#discazos-player-html");
   
   DiscazosPlayer.data = { 
-    albumLength: DiscazosPlayer.html.find("#album-length").html(), 
+    albumLength: DiscazosPlayerUI.html.find("#album-length").html(), 
     currentTrack: false,
     minLoadedTriggered: false,
   };
   
   /** Initialize UI **/
-  
-  $("#play-button").button({
+ DiscazosPlayerUI.init();
+
+});
+
+/* Player UI */
+
+DiscazosPlayerUI.init = function() {
+  //Setup player control buttons
+  this.html.find("#play-button").button({
     text: false,
     icons: {
       primary: "ui-icon-play"
     }
   }).click(function() {
-    var options;
     if($(this).text() === "Play") {
-      DiscazosPlayer.play();
-      options = {
-        label: "Pause",
-        icons: {
-          primary: "ui-icon-pause"
-        }
-      };
+      DiscazosPlayer.pressPlay();
     } else {
-      DiscazosPlayer.pause();
-      options = {
-        label: "Play",
-        icons: {
-          primary: "ui-icon-play"
-        }
-      };
+      DiscazosPlayer.pressPause();
     }
-    $(this).button("option", options);
   });
-  $("#prev-button").button({
+  this.html.find("#prev-button").button({
     text: false,
     icons: {
       primary: "ui-icon-seek-start"
-    }
+    },
+    disabled: true,
   }).click(function() { 
     DiscazosPlayer.playPrevious(); 
   });
-  $("#next-button").button({
+  this.html.find("#next-button").button({
     text: false,
     icons: {
       primary: "ui-icon-seek-end"
-    }
+    },
+    disabled: true,
   }).click(function() { 
     DiscazosPlayer.playNext(); 
   });
   
-  DiscazosPlayer.html.find("#player-wrapper div.main div.left").alternateScroll();
+  //Setup playlist scrolling
+  this.html.find("#player-wrapper div.main div.left").alternateScroll();
   
-  DiscazosPlayer.html.find("#loading-discazo").progressbar();
+  //Setup loading progressbar
+  this.html.find("#loading-discazo").progressbar();
   
-  DiscazosPlayer.html.find("#progressbar").slider({
+  //Setup track seek/progress bar
+  this.html.find("#progressbar").slider({
     range: 'min',
     change: function(event, ui) {
       if(event.originalEvent && event.originalEvent.type == "mouseup") {
@@ -77,41 +76,82 @@ $(document).ready(function() {
       $(this).data('is_being_slided', false);
     }
   }).data('is_being_slided', false);
-  
-  DiscazosPlayer.html.find(".playlist tr.track").each(function() {
-    $(this).click(function() { DiscazosPlayer.playTrack(this); });
-  });
+}
 
-});
+DiscazosPlayerUI.setButtonPlay = function() {
+  var options = {
+    label: "Play",
+    icons: {
+      primary: "ui-icon-play"
+    }
+  };
+  this.html.find("#play-button").button("option", options);
+}
+
+DiscazosPlayerUI.setButtonPause = function() {
+  var options = {
+    label: "Pause",
+    icons: {
+      primary: "ui-icon-pause"
+    }
+  }
+  this.html.find("#play-button").button("option", options);
+}
+
+DiscazosPlayerUI.disablePrevButton = function() {
+  this.html.find("#prev-button").button("disable");
+}
+
+DiscazosPlayerUI.disableNextButton = function() {
+  this.html.find("#next-button").button("disable");
+}
+
+DiscazosPlayerUI.enablePrevButton = function() {
+  this.html.find("#prev-button").button("enable");
+}
+
+DiscazosPlayerUI.enableNextButton = function() {
+  this.html.find("#next-button").button("enable");
+}
 
 /***** JS Player ******/
 
 /* Actions */
 
 DiscazosPlayer.load = function(url) {
-  var albumCover = DiscazosPlayer.html.find("a.load-link");
+  var albumCover = DiscazosPlayerUI.html.find("a.load-link");
   albumCover.find("span.load-button").remove();
   albumCover.find("#album-cover").unwrap(); //Removes the loading link
   
   this.swf.load(url);
   
-  DiscazosPlayer.html.find('#loading-discazo span.caption').show();
+  DiscazosPlayerUI.html.find('#loading-discazo span.caption').show();
   
-  DiscazosPlayer.html.bind("minLoaded", function() {
-    DiscazosPlayer.html.find("#player-controls").slideDown('slow');
+  //After the a minimum part has been loaded, activate the player controls
+  DiscazosPlayerUI.html.bind("minLoaded", function() {
+    DiscazosPlayerUI.html.find("#player-controls").slideDown('slow');
+    DiscazosPlayerUI.html.find(".playlist tr.track").each(function() {
+      $(this).hover(function() { $(this).addClass("hover") }, 
+                    function() { $(this).removeClass("hover") });
+      $(this).click(function() { DiscazosPlayer.playTrack(this); });
+    });
   });
 }
 
-DiscazosPlayer.play = function() {
+DiscazosPlayer.pressPlay = function() {
+  //Play first tracks if there's no current, 
+  //else continue playing the current
   if(!this.data.currentTrack) {
-    this.playTrack(this.html.find('.playlist tr.track:first'))
+    this.playTrack(DiscazosPlayerUI.html.find('.playlist tr.track:first'))
   } else {
+    DiscazosPlayerUI.setButtonPause();
     this.swf.play();
   }
 }
 
-DiscazosPlayer.pause = function() {
+DiscazosPlayer.pressPause = function() {
   this.swf.pause();
+  DiscazosPlayerUI.setButtonPlay();
 }
 
 DiscazosPlayer.playTrack = function(trackItem) {
@@ -120,9 +160,10 @@ DiscazosPlayer.playTrack = function(trackItem) {
   
   //Start playing the track
   newMsPosition = this.data.currentTrack.offset * 1000;
-  //If it's the starting position, seek to 1
-  if(newMsPosition == 0) newMsPosition = 1;
+  if(newMsPosition == 0) newMsPosition = 1; //If it's the starting position, seek to 1
   this.swf.seek(newMsPosition);
+  
+  DiscazosPlayerUI.setButtonPause();
 }
 
 DiscazosPlayer.seekTrack = function(seekPercentage) {
@@ -130,6 +171,7 @@ DiscazosPlayer.seekTrack = function(seekPercentage) {
   var seekOffset = Math.round(this.data.currentTrack.length * seekPercentage / 100);
   var newMsPosition = (trackStart + seekOffset) * 1000;
   this.swf.seek(newMsPosition);
+  DiscazosPlayerUI.setButtonPause();
 }
 
 DiscazosPlayer.playNext = function() {
@@ -140,6 +182,14 @@ DiscazosPlayer.playPrevious = function() {
   this.playTrack($(this.data.currentTrack.item).prev());
 }
 
+DiscazosPlayer.currentTrackIsLast = function() {
+  return $(this.data.currentTrack.item).next().length == 0;
+}
+
+DiscazosPlayer.currentTrackNumber = function() {
+  return this.data.currentTrack.item.find('.track-number').html();
+}
+
 DiscazosPlayer.currentTrackChanged = function(trackItem) {
   this.data.currentTrack = { 
     item: $(trackItem),
@@ -147,9 +197,20 @@ DiscazosPlayer.currentTrackChanged = function(trackItem) {
     length: parseInt($(trackItem).find('.track-length').attr('rel')),
   }
   //Set progress bar to 0
-  this.html.find('#progressbar').progressbar("value", 0);
+  DiscazosPlayerUI.html.find('#progressbar').progressbar("value", 0);
   //Update track name
-  this.html.find('#current-track').html($(trackItem).find('.track-name').html());
+  DiscazosPlayerUI.html.find('#current-track').html($(trackItem).find('.track-name').html());
+  
+  if(this.currentTrackNumber() == 1) {
+    DiscazosPlayerUI.disablePrevButton();
+    DiscazosPlayerUI.enableNextButton();
+  } else if(this.currentTrackIsLast()) {
+    DiscazosPlayerUI.disableNextButton();
+    DiscazosPlayerUI.enablePrevButton();
+  } else {
+    DiscazosPlayerUI.enablePrevButton();
+    DiscazosPlayerUI.enableNextButton();
+  }
 }
 
 DiscazosPlayer.updateTrackProgress = function(currentProgress) {
@@ -158,7 +219,7 @@ DiscazosPlayer.updateTrackProgress = function(currentProgress) {
 }
 
 DiscazosPlayer.updateTrackProgressbar = function(currentProgress) {
-  var progressbar = this.html.find('#progressbar');
+  var progressbar = DiscazosPlayerUI.html.find('#progressbar');
   if(!progressbar.data('is_being_slided')) { //This is to prevent the slider to 
                                             //move while being slided for seeking
     var percentage = Math.round((currentProgress / this.data.currentTrack.length) * 100);
@@ -167,7 +228,18 @@ DiscazosPlayer.updateTrackProgressbar = function(currentProgress) {
 }
 
 DiscazosPlayer.updateTrackTimeElapsed = function(currentProgress) {
-  this.html.find('#time-elapsed').html(this.convertToMMSS(currentProgress));
+  DiscazosPlayerUI.html.find('#time-elapsed').html(this.convertToMMSS(currentProgress));
+}
+
+DiscazosPlayer.stop = function() {
+  this.swf.pause();
+  DiscazosPlayerUI.disableNextButton();
+  DiscazosPlayerUI.disablePrevButton();
+  this.updateTrackTimeElapsed(0);
+  this.updateTrackProgressbar(0);
+  DiscazosPlayerUI.html.find('#current-track').html("");
+  this.data.currentTrack = false;
+  DiscazosPlayerUI.setButtonPlay();
 }
 
 /* Aux */
@@ -187,12 +259,12 @@ DiscazosPlayer.convertToMMSS = function(seconds) {
 DiscazosPlayer.updateBufferProgress = function(msLoaded, msTotal) {
   var secondsLoaded = msLoaded / 1000;
   var percentage = Math.round((secondsLoaded / this.data.albumLength) * 100);
-  var progressbarWidget = this.html.find('#loading-discazo');
+  var progressbarWidget = DiscazosPlayerUI.html.find('#loading-discazo');
   progressbarWidget.progressbar("value", percentage);
   if(percentage >= DiscazosPlayer.MIN_PERCENTAGE_TO_LOAD) {
     if(!this.data.minLoadedTriggered) {
        this.data.minLoadedTriggered = true;
-       this.html.trigger("minLoaded");
+       DiscazosPlayerUI.html.trigger("minLoaded");
     }
     progressbarWidget.children('span.caption').html(percentage + '%');
   }
@@ -203,7 +275,11 @@ DiscazosPlayer.currentPlaybackPositionChanged = function(currentMs) {
   var currentTrackProgress = currentSeconds - this.data.currentTrack.offset;
   var currentTrackHasFinish = currentTrackProgress >= this.data.currentTrack.length;
   if(currentTrackHasFinish) {
-    this.currentTrackChanged($(this.data.currentTrack.item).next());
+    if(!this.currentTrackIsLast) {
+      this.currentTrackChanged($(this.data.currentTrack.item).next());
+    } else {
+      DiscazosPlayer.stop();
+    }
   } else {
     this.updateTrackProgress(currentTrackProgress);
   }
